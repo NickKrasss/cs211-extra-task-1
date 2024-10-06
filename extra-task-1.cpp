3b746d3559fe673ac011855726b6961ba18b73d2
NickKrasss <145866917+NickKrasss@users.noreply.github.com>
2024-10-06 22:24:13 +0300

    complete main and tests

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -73,4 +73,54 @@ double time_from_utc(int utc_offset, double time)
 }
 
 
+int main() {
 
+    double eps = DBL_EPSILON;
+
+    // Тест задачи 1
+    assert(abs(seconds_difference(1000.0, 2000.0) - 1000.0) < eps);
+    assert(abs(seconds_difference(2000.0, 1000.0) - -1000.0) < eps);
+    assert(abs(seconds_difference(1000.0, 1500.0) - 500.0) < eps);
+    assert(abs(seconds_difference(1000.0, 1000.0)) < eps);
+
+    // Тест задачи 2
+    assert(abs(hours_difference(1000.0, 2000.0) - 0.2777777777777778) < eps);
+    assert(abs(hours_difference(2000.0, 1000.0) - -0.2777777777777778) < eps);
+    assert(abs(hours_difference(1000.0, 1500.0) - 0.1388888888888889) < eps);
+    assert(abs(hours_difference(1000.0, 1000.0)) < eps);
+
+    // Тест задачи 3
+    assert(abs(to_float_hours(1, 30, 0) - 1.5) < eps);
+    assert(abs(to_float_hours(3, 45, 18) - 3.755) < eps);
+    assert(abs(to_float_hours(2, 0, 45) - 2.0125) < eps);
+
+    // Тест задачи 4
+    assert(abs(to_24_hour_clock(24)) < eps);
+    assert(abs(to_24_hour_clock(48)) < eps);
+    assert(abs(to_24_hour_clock(25) - 1) < eps);
+    assert(abs(to_24_hour_clock(5) - 5) < eps);
+    assert(abs(to_24_hour_clock(29.5) - 5.5) < eps);
+
+    // Тест задачи 5
+    assert(get_hours(4000) == 1);
+    assert(get_minutes(4000) == 6);
+    assert(get_seconds(4000) == 40);
+
+    // Тест задачи 6
+    assert(abs(time_to_utc(0, 13.0) - 13.0) < eps);
+    assert(abs(time_to_utc(+2, 13.0) - 11.0) < eps);
+    assert(abs(time_to_utc(-2, 13.0) - 15.0) < eps);
+    assert(abs(time_to_utc(-12, 19.0) - 7.0) < eps);
+    assert(abs(time_to_utc(-2, 1.0) - 3.0) < eps);
+
+    // Тест задачи 7
+    assert(abs(time_from_utc(+0, 13.0) - 13.0) < eps);
+    assert(abs(time_from_utc(+2, 13.0) - 15.0) < eps);
+    assert(abs(time_from_utc(-2, 13.0) - 11.0) < eps);
+    assert(abs(time_from_utc(+7, 7.0) - 14.0) < eps);
+    assert(abs(time_from_utc(-8, 7.0) - 23.0) < eps);
+    assert(abs(time_from_utc(-2, 1.0) - 23.0) < eps);
+    assert(abs(time_from_utc(-2, 23.0) - 21.0) < eps);
+
+    return 0;
+}
