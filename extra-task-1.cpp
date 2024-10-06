1af566c72dc017807a1bfb3343d26e7814295aa9
NickKrasss <145866917+NickKrasss@users.noreply.github.com>
2024-10-06 22:21:53 +0300

    complete function to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -13,3 +13,13 @@ double hours_difference(double time_1, double time_2)
     return (time_2 - time_1) / 3600.0;
 }
 
+// Функция для преобразования времени в формат с плавающей запятой (часы)
+double to_float_hours(int hours, int minutes, int seconds)
+{
+    assert(minutes >= 0 && minutes <= 60);
+    assert(seconds >= 0 && seconds <= 60);
+
+    return hours + (minutes / 60.0) + (seconds / 3600.0);
+}
+
+
