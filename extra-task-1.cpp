69fe8e4c268210231251881c1128a54dab5529f1
NickKrasss <145866917+NickKrasss@users.noreply.github.com>
2024-10-06 22:21:32 +0300

    complete function hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -7,4 +7,9 @@ double seconds_difference(double time_1, double time_2)
     return time_2 - time_1;
 }
 
+// Функция для вычисления разницы во времени в часах
+double hours_difference(double time_1, double time_2)
+{
+    return (time_2 - time_1) / 3600.0;
+}
 
